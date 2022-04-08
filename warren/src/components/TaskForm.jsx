3a07864dea6236fbd8ce636c000f9a0d83c95cf7
DamianOzuna <damianozuna9@gmail.com>
2022-04-07 21:00:48 -0500

    updated taskform component

--- warren/src/components/TaskForm.jsx
+++ warren/src/components/TaskForm.jsx
@@ -3,17 +3,19 @@
 const TaskForm = () => {
     return (
         <div class="container">
-            <form class="form-inline justify-content-center">
-                <div class="form-group col-7">
-                    <input class="form-control col-12" type="text" 
-                        placeholder="Create a new task"/>
-                </div>
-                <div class="form-group col-xs-2">
-                    <input type="date" class="form-control" id="inlineFormInput"
-                                placeholder="Deadline"/>
-                </div>
-                <div class="form-group col-3">
-                    <button type="submit" class="form-control btn btn-primary ">Submit</button>
+            <form>
+                <div class="form-row">
+                    <div class="col-md-8 mb-3">
+                        <input class="form-control" type="text" 
+                            placeholder="Create a new task"/>
+                    </div>
+                    <div class="col-md-2 mb-3">
+                        <input type="date" class="form-control" id="inlineFormInput"
+                                    placeholder="Deadline"/>
+                    </div>
+                    <div class="col-md-2 mb-3">
+                        <button type="submit" class="form-control btn btn-primary ">Submit</button>
+                    </div>
                 </div>
             </form>
         </div>
