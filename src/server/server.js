32bd7138f498fef0de237851344aa2966f51cccc
Aiden Escamilla <aiden.escamilla@gmail.com>
2023-04-27 15:13:41 -0500

    bad query returns all rows

--- src/server/server.js
+++ src/server/server.js
@@ -175,7 +175,7 @@ app.get("/EmployeeNewHire", async (req, res) => {
 // Use this as a template to make your own queries
 app.get("/DefaultTaskList", async (req, res) => {
     try{
-        const results = await client.query("select task_description, department_name, deadline, member_assigned  from public.task_list WHERE department_name = \'Basic Onboarding\'");
+        const results = await client.query("select Distinct(task_description), department_name, deadline, member_assigned  from public.task_list WHERE department_name = \'Basic Onboarding\'");
         res.json(results);
     }catch(e){
         console.error(`query failed ${e}`);
