9be9d9740f891466e6985850bf981f0687daa2b0
TusharW4ni <reachtusharwani@gmail.com>
2023-12-03 22:57:08 -0600

    updated profile tab

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,61 +1,86 @@
+// import React from 'react';
+// import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+// import Departments from '../../pages/departments';
+// import Users from '../../pages/users';
+// import Archive from '../../pages/archive';
+// import LoginButton from '../LogIn';
+// import LogoutButton from '../Logout';
+// import Profile from '../Profile';
+// import { Link } from 'react-router-dom';
+
+// const Navbar = () => {
+// 	return (
+// 		<Tabs isLazy>
+// 			{/* <TabList>
+// 				<Tab>Users</Tab>
+// 				<Tab>Departments</Tab>
+// 				<Tab>Archive</Tab>
+// 				<Tab>LogIn</Tab>
+// 				<Tab>LogOut</Tab>
+// 				<Tab>Profile</Tab>
+// 			</TabList> */}
+
+// 			<TabList>
+// 				<Tab><Link to="/users">Users</Link></Tab>
+// 				<Tab><Link to="/departments">Departments</Link></Tab>
+// 				<Tab><Link to="/archive">Archive</Link></Tab>
+// 				{/* <Tab><Link to="/login">LogIn</Link></Tab> */}
+// 				{/* <Tab><Link to="/logout">Log Out</Link></Tab> */}
+// 				<Tab><Link to="/profile">Profile</Link></Tab>
+// 			</TabList>
+		
+// 			{/* <TabPanels>
+
+// 				<TabPanel>
+// 					<Users />
+// 				</TabPanel>
+
+// 				<TabPanel>
+// 					<Departments />
+// 				</TabPanel>
+
+// 				<TabPanel>
+// 					<Archive />
+// 				</TabPanel>
+
+// 				<TabPanel>
+// 					<LoginButton />
+// 				</TabPanel>
+
+// 				<TabPanel>
+// 					<LogoutButton />
+// 				</TabPanel>
+
+// 				<TabPanel>
+// 					<Profile />
+// 				</TabPanel>
+
+// 			</TabPanels> */}
+// 		</Tabs>
+// 	);
+// };
+
+// export default Navbar;
+
+
 import React from 'react';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import Departments from '../../pages/departments';
-import Users from '../../pages/users';
-import Archive from '../../pages/archive';
-import LoginButton from '../LogIn';
-import LogoutButton from '../Logout';
+import { Tabs, TabList, Tab, Button, useDisclosure } from '@chakra-ui/react'
 import Profile from '../Profile';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
 	return (
 		<Tabs isLazy>
-			{/* <TabList>
-				<Tab>Users</Tab>
-				<Tab>Departments</Tab>
-				<Tab>Archive</Tab>
-				<Tab>LogIn</Tab>
-				<Tab>LogOut</Tab>
-				<Tab>Profile</Tab>
-			</TabList> */}
-
 			<TabList>
 				<Tab><Link to="/users">Users</Link></Tab>
 				<Tab><Link to="/departments">Departments</Link></Tab>
 				<Tab><Link to="/archive">Archive</Link></Tab>
-				{/* <Tab><Link to="/login">LogIn</Link></Tab> */}
-				<Tab><Link to="/logout">LogOut</Link></Tab>
-				<Tab><Link to="/profile">Profile</Link></Tab>
+				<Button onClick={onOpen}>Profile</Button>
 			</TabList>
-		
-			{/* <TabPanels>
 
-				<TabPanel>
-					<Users />
-				</TabPanel>
-
-				<TabPanel>
-					<Departments />
-				</TabPanel>
-
-				<TabPanel>
-					<Archive />
-				</TabPanel>
-
-				<TabPanel>
-					<LoginButton />
-				</TabPanel>
-
-				<TabPanel>
-					<LogoutButton />
-				</TabPanel>
-
-				<TabPanel>
-					<Profile />
-				</TabPanel>
-
-			</TabPanels> */}
+			<Profile isOpen={isOpen} onClose={onClose} />
 		</Tabs>
 	);
 };
