81f530b2be01c249210c85e555c447fc91434dce
ssamikshaa <samikshaa.saiprakash@gmail.com>
2024-03-27 16:37:02 -0500

    added archive task API calls and updated schema

--- updatedApp/backend/server/tasks/subRouter.js
+++ updatedApp/backend/server/tasks/subRouter.js
@@ -2,13 +2,26 @@ const controller = require('./tasksController');
 
 const addRoutes = router => {
   //GET
-  router.get('/allTasksForEmployee/:id', controller.getAllTasksForEmployee);
-  router.get('/allTasksForSupervisor/:id', controller.getAllTasksForSupervisor);
+  router.get('/getAllTasksForEmployee/:id', controller.getAllTasksForEmployee);
+  router.get('/getAllTasksForSupervisor/:id', controller.getAllTasksForSupervisor);
 
   //POST
   router.post('/addTaskForDepartment', controller.addTaskForDepartment);
   router.post('/addTaskForSupervisor', controller.addTaskForSupervisor);
   router.post('/addTaskForEmployee', controller.addTaskForEmployee); 
+
+  //PUT
+  router.put('/archiveTaskForEmployee', controller.archiveTaskForEmployee);
+  router.put('/archiveTaskForSupervisor', controller.archiveTaskForSupervisor);
+  router.put('/archiveTaskForDepartment', controller.archiveTaskForDepartment);
+  
+//   router.put('/task/update-employee-task/:id', controller.updateEmployeeTask);
+//   router.put('/task/update-department-task/:id', controller.updateDepartmentTask);  
+  
+//   //PATCH
+//   router.patch('/task/complete-task/', controller.completeTask);
+//   router.patch('/task/uncomplete-task/', controller.uncompleteTask);
+
 };
 
 module.exports = {
