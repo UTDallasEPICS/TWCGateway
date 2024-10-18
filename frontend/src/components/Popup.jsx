e57967f36fea1896a57791640880b340b47c1f3a
ja248230 <146367178+ja248230@users.noreply.github.com>
2024-10-18 18:08:53 -0500

    wtv

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Popup({ trigger, setTrigger, children }) {
   return (
     trigger ? (
-      <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
-        <div className="bg-white p-6 rounded-lg shadow-lg relative w-full max-w-lg mx-4">
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white p-6 rounded-lg shadow-lg relative w-full max-w-lg mx-4">
           <button 
             className="absolute top-2 right-3 text-gray-600 hover:text-red-600 focus:outline-none" 
             onClick={() => setTrigger(false)}>
